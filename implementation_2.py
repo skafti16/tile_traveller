4f5552607861b273ab9b9016ccc15bc7ad3cd81d
skafti16 <skafti16@ru.is>
2018-09-19 22:40:54 +0000

    virk stutt utfærsla

--- implementation_2.py
+++ implementation_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #1-skilgreina breytur x_cord og y_cord. Og input option 
 #2-while loop  með if statements innbyrgðis
 #3-Victory!
@@ -9,48 +8,59 @@ x_cord,y_cord = 1,1
 
 #byrjun a loop-u 
 while x_cord != 3 or y_cord != 1:
-    print('You can travel:', end=' ')
-    
-    if x_cord == 1 and y_cord == 1:
+    if (x_cord == 1 and y_cord == 1) or (x_cord == 2 and y_cord == 1):
         is_open = 'n'
+    elif x_cord == 1 and y_cord == 2:
+        is_open = 'nes'
+    elif x_cord == 1 and y_cord == 3:
+        is_open = 'es'
+    elif x_cord == 2 and y_cord == 2:
+        is_open = 'sw'
+    elif x_cord == 2 and y_cord == 3:
+        is_open = 'ew'
+    elif x_cord == 3 and y_cord == 3:
+        is_open = 'sw'
+    elif x_cord == 3 and y_cord == 2:
+        is_open = 'ns'
+        
+    print('You can travel:', end=' ')
+        
+    for i in range(len(is_open)):
+        if i > 0:
+            print('or',end=' ') 
 
-        for i in range(len(is_open)):
-            if i > 0:
-                print('or',end=' ') 
-                
-            if is_open[i] == 'n':
-                print('(N)orth', end='')
-            elif is_open[i] == 's':
-                print('(S)outh', end='')
-            elif is_open[i] == 'e':
-                print('(E)ast', end='')
-            elif is_open[i] == 'w': 
-                print('(W)est', end='') 
-            if i == len(is_open) - 1: 
-                print('.')
-            else:
-                print(' ', end='')
+        if is_open[i] == 'n':
+            print('(N)orth', end='')
+        elif is_open[i] == 's':
+            print('(S)outh', end='')
+        elif is_open[i] == 'e':
+            print('(E)ast', end='')
+        elif is_open[i] == 'w': 
+            print('(W)est', end='') 
+        if i == len(is_open) - 1: 
+            print('.')
+        else:
+            print(' ', end='')
 
-        to_direction = input('Direction: ')
-        to_direction = to_direction.lower()
-        while to_direction not in is_open:
+    to_direction = input('Direction: ')
+    to_direction = to_direction.lower()
+    while to_direction not in is_open:
             print('Not a valid direction!')
             to_direction = input('Direction: ')
             to_direction = to_direction.lower()
 
-        else:
-            if to_direction == 'n':
-                y_cord += 1
-                continue
-            if to_direction == 's':
-                y_cord -= 1
-                continue
-            if to_direction == 'w':
-                x_cord -= 1
-                continue
-            if to_direction == 'e':
-                x_cord += 1
-                continue          
-          
-else:
+    else:
+        if to_direction == 'n':
+            y_cord += 1
+            continue
+        elif to_direction == 's':
+            y_cord -= 1
+            continue
+        elif to_direction == 'w':
+            x_cord -= 1
+            continue
+        elif to_direction == 'e':
+            x_cord += 1
+            continue
+else: 
     print('Victory!')
